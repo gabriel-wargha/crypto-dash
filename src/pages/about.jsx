20fb03217f5e39f2521ac92afc31d34fef90949a
gabriel-wargha <gabriel@wargha.com>
2025-12-08 16:11:12 -0700

    Adding routes

---
+++ src/pages/about.jsx
@@ -0,0 +1,24 @@
+import Header from '../components/Header';
+const AboutPage = () => {
+	return (
+		<>
+			<Header />
+
+			<div className="about">
+				<h1>About Crypto Dash</h1>
+				<p>
+					This platform is designed to help crypto investors make informed
+					decisions. It provides clear, reliable information on market trends,
+					prices, and key data—everything needed to stay up to date without
+					distractions or clutter. The focus is on simplicity and clarity,
+					giving investors a steady, trustworthy tool to monitor the crypto
+					world and act with confidence. Whether checking the market daily or
+					reviewing it occasionally, this site keeps information organized,
+					accessible, and easy to understand.
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default AboutPage;
